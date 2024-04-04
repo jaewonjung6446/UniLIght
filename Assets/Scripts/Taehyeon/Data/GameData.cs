52f9b814332e31f547e474b990455b2c3641ecf9
jaewonjung6446 <jaewon6446@naver.com>
2024-04-04 12:48:14 +0900

    00

--- Assets/Scripts/Taehyeon/Data/GameData.cs
+++
@@ -1,7 +0,0 @@
-public static class GameData
-{
-    public static readonly int playerNumPerTeam = 1;
-    public static readonly float initialPlayTime = 60;
-    
-    public static int currentConnectedPlayerNum = 0;
-}
--- Assets/Scripts/Taeseung/Weapon/ParentClass/ShortDistanceWeaponManager.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class ShortDistanceWeaponManager : MainWeaponManager
-{
-    [SerializeField] protected float _weaponAttackReach;
-}
