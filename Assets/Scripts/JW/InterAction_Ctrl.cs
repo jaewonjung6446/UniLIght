8d60e8042e4a26ab14e6329dcda12a930a004fbf
jaewonjung6446 <jaewon6446@naver.com>
2024-04-22 12:16:20 +0900

    텍스트 출력 상호작용

--- Assets/Scripts/JW/InterAction_Ctrl.cs
+++ Assets/Scripts/JW/InterAction_Ctrl.cs
@@ -1,16 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InterAction_Ctrl : MonoBehaviour
 {
-    public float raycastDistance = 3f; //인식할 수 있는 범위
+    public float raycastDistance = 6f; //인식할 수 있는 범위
+    public Text pauseText; // Inspector에서 할당
+    public string[] messages; // 표시할 메시지 배열
+    private int currentIndex = 0; // 현재 메시지 인덱스
 
     RaycastHit hit;
     Ray ray;
 
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            ToggleText();
+        }
         Debug.DrawLine(ray.origin, ray.origin + ray.direction * raycastDistance, Color.red); //씬에서 내가 보고있는 방향을 표시
 
         ray = new Ray(transform.position, transform.forward); //보고있는 방향으로 살펴보기
@@ -27,10 +35,26 @@ public class InterAction_Ctrl : MonoBehaviour
                 {
                     if (hitObject.GetComponent<InventoryManager>() != null)
                     {
+                        //ToggleText();
                         hitObject.GetComponent<InventoryManager>().AddToInventory(hitObject);
                     }
                 }
             }
+        }
+    }
+    void ToggleText()
+    {
+        if (currentIndex < messages.Length)
+        {
+            Time.timeScale = 0f; // 게임 일시 정지
+            pauseText.text = messages[currentIndex]; // 현재 인덱스의 메시지 표시
+            pauseText.gameObject.SetActive(true); // 텍스트 활성화
+            currentIndex++; // 다음 메시지로 인덱스 증가
+        }
+        else if(Time.timeScale ==0 && currentIndex == messages.Length)
+        {
+            Time.timeScale = 1.0f; // 게임 재개
+            pauseText.gameObject.SetActive(false); // 텍스트 숨기기
         }
     }
 }
