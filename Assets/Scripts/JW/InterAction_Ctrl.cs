7e0c338b187ebec9908c2a3e9bca5fbc551fb3e9
jaewonjung6446 <jaewon6446@naver.com>
2024-04-22 12:58:53 +0900

    텍스트출력 버그수정

--- Assets/Scripts/JW/InterAction_Ctrl.cs
+++ Assets/Scripts/JW/InterAction_Ctrl.cs
@@ -7,18 +7,15 @@ public class InterAction_Ctrl : MonoBehaviour
 {
     public float raycastDistance = 6f; //인식할 수 있는 범위
     public Text pauseText; // Inspector에서 할당
+    [TextArea]
     public string[] messages; // 표시할 메시지 배열
     private int currentIndex = 0; // 현재 메시지 인덱스
-
+    private bool toggleText = false;
     RaycastHit hit;
     Ray ray;
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            ToggleText();
-        }
         Debug.DrawLine(ray.origin, ray.origin + ray.direction * raycastDistance, Color.red); //씬에서 내가 보고있는 방향을 표시
 
         ray = new Ray(transform.position, transform.forward); //보고있는 방향으로 살펴보기
@@ -27,19 +24,23 @@ public class InterAction_Ctrl : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E)) //키보드 E를 눌렀을 때
         {
+            GameObject hitObject;
             if (Physics.Raycast(ray, out hit, raycastDistance)) //인식할 수 있는 범위 안에서 물체 확인
             {
-                GameObject hitObject = hit.collider.gameObject; //주변 물체의 정보를 가져옵니다.
+                hitObject = hit.collider.gameObject; //주변 물체의 정보를 가져옵니다.
 
                 if (hitObject != null) //물체가 있을 경우
                 {
                     if (hitObject.GetComponent<InventoryManager>() != null)
                     {
-                        //ToggleText();
                         hitObject.GetComponent<InventoryManager>().AddToInventory(hitObject);
+                        toggleText = true;
                     }
                 }
             }
+        }
+        if (Input.GetKeyDown(KeyCode.E) && toggleText){
+            ToggleText();
         }
     }
     void ToggleText()
@@ -55,6 +56,7 @@ public class InterAction_Ctrl : MonoBehaviour
         {
             Time.timeScale = 1.0f; // 게임 재개
             pauseText.gameObject.SetActive(false); // 텍스트 숨기기
+            toggleText = false;
         }
     }
 }
