a7a458f8eab7cb1458a99b60b3f3449b093d386a
jaewonjung6446 <jaewon6446@naver.com>
2024-05-08 11:55:01 +0900

    상호작용 리팩토링 및 씬 전환 기능 추가
    1.상호작용 리팩토링: 이전에 Update상에서 모두 처리하던 것을 조건을 만족시키면 코루틴을 호출하는 방식으로 바꿈. 실시간 자원은 조금 더 잡아먹겠지만, 유지보수가 쉬울 것 같아서 리팩토링.
    2.씬 전환 기능 추가: 추가라고 할 것도 없긴 한데... 그냥 특정 오브젝트 ray로 인식하면 씬이 전환되는 기능임.
    3.구현 예정: 씬 전환에서 부드럽게 lerp로 전환하는 기능(실제로는 그렇게 보이게만 하는 것이지만...)

--- Assets/Scripts/JW/InterAction_Ctrl.cs
+++ Assets/Scripts/JW/InterAction_Ctrl.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class InterAction_Ctrl : MonoBehaviour
 {
-    public float raycastDistance = 6f; //인식할 수 있는 범위
+    public float raycastDistance = 10f; //인식할 수 있는 범위
     public Text pauseText; // Inspector에서 할당
     [TextArea]
     public string[] Obj_Cube; // 표시할 메시지 배열
@@ -27,61 +28,16 @@ public class InterAction_Ctrl : MonoBehaviour
     RaycastHit hit;
     Ray ray;
     [SerializeField] private Camera mainCam;
-
     void Update()
     {
+
         Debug.DrawLine(ray.origin, ray.origin + ray.direction * raycastDistance, Color.red); //씬에서 내가 보고있는 방향을 표시
 
         ray = new Ray(transform.position, transform.forward); //보고있는 방향으로 살펴보기
         GetInfo();
         DoWhat();
-        if (toggleText)
-        {
-            //Debug.Log(currentIndex.ToString());
-            ToggleText(GetInfo());
-        }
-        if (lerpPOV)
-        {
-            TurnPOV();
-        }
-    }
-    private void LockCursor()
-    {
-        Cursor.lockState = CursorLockMode.Locked;
-        Cursor.visible = false;
-    }
-
-    private void UnlockCursor()
-    {
-        Cursor.lockState = CursorLockMode.None;
-        Cursor.visible = true;
-    }
-    //텍스트 순차 출력 메소드, message안에 출력할 텍스트 배열을 입력하면 추적해서 Update에서 출력함
-    void ToggleText(GameObject interActionObj)
-    {
-        if (Input.GetKeyDown(KeyCode.E))
-        {
-            switch (interActionObj.name)
-            {
-                case "Obj_Cube":
-                    Debug.Log(currentIndex < Obj_Cube.Length);
-                    if (currentIndex < Obj_Cube.Length)
-                    {
-                        Debug.Log("작동 중");
-                        Time.timeScale = 0f; // 게임 일시 정지
-                        pauseText.text = Obj_Cube[currentIndex]; // 현재 인덱스의 메시지 표시
-                        pauseText.gameObject.SetActive(true); // 텍스트 활성화
-                        currentIndex++; // 다음 메시지로 인덱스 증가
-                    }
-                    else if (Time.timeScale == 0 && currentIndex == Obj_Cube.Length)
-                    {
-                        Time.timeScale = 1.0f; // 게임 재개
-                        pauseText.gameObject.SetActive(false); // 텍스트 숨기기
-                        toggleText = false;
-                    }
-                    break;
-            }
-        }
+        if (lerpPOV) StartCoroutine(TurnPOV());
+        if (toggleText) StartCoroutine(ToggleText()); // 코루틴 시작
     }
     GameObject GetInfo()
     {
@@ -90,7 +46,7 @@ public class InterAction_Ctrl : MonoBehaviour
             if (Physics.Raycast(ray, out hit, raycastDistance)) //인식할 수 있는 범위 안에서 물체 확인
             {
                 hitObject = hit.collider.gameObject; //주변 물체의 정보를 가져옵니다. 상호작용 중임을 나타내는 bool을 true로 합니다.
-                if(hitObject != null)
+                if (hitObject != null)
                 {
                     interAction = true;
                 }
@@ -102,12 +58,13 @@ public class InterAction_Ctrl : MonoBehaviour
     {
         if (hitObject != null && interAction)
         {
-            if (hitObject.GetComponent<InventoryManager>() != null)
+            if (hitObject.name == "Obj_Cube")
             {
-                hitObject.GetComponent<InventoryManager>().AddToInventory(hitObject);
+                
+                if(hitObject.GetComponent<InventoryManager>() !=null) hitObject.GetComponent<InventoryManager>().AddToInventory(hitObject);
                 toggleText = true;
             }
-            if (hitObject.CompareTag("Flag"))
+            if (hitObject.name == "Flag")
             {
                 Debug.Log("물체감지");
                 startPosition = transform.position;
@@ -120,18 +77,34 @@ public class InterAction_Ctrl : MonoBehaviour
                 journeyLength = Vector3.Distance(startPosition, endPosition); // 시작점과 종점 사이의 거리 계산
                 startTime = Time.time;
                 GetComponentInChildren<CameraSettings>().enabled = false;
-                LockCursor();
+                lerpPOV = true;
+            }
+            if (hitObject.name == "Flag2")
+            {
+                Debug.Log("물체감지2");
+                startPosition = transform.position;
+                startRotation = transform.rotation;
+                if (GetInfo() != null)
+                {
+                    endPosition = new Vector3(0.340000004f, 0.996999979f, -0.949999988f);      // 목표 Transform의 위치를 종료 위치로 설정
+                    endRotation = GetInfo().transform.rotation;       // 목표 Transform의 회전을 종료 회전으로 설정
+                }
+                journeyLength = Vector3.Distance(startPosition, endPosition); // 시작점과 종점 사이의 거리 계산
+                startTime = Time.time;
+                GetComponentInChildren<CameraSettings>().enabled = false;
                 lerpPOV = true;
             }
         }
         interAction = false;
     }
 
-    private void TurnPOV()
+    IEnumerator TurnPOV()
     {
-        // 보간 중의 거리를 로그로 출력
         Debug.Log((transform.position - endPosition).magnitude);
-
+        if((transform.position - endPosition).magnitude <= 0.3f && hitObject.name == "Flag2")
+        {
+            SceneManager.LoadScene("SceneTranformTest");
+        }
         if ((transform.position - endPosition).magnitude > 0.1f)
         {
             // 이동 중
@@ -139,6 +112,7 @@ public class InterAction_Ctrl : MonoBehaviour
             float fractionOfJourney = distCovered / journeyLength;
             transform.position = Vector3.Lerp(startPosition, endPosition, fractionOfJourney);
             transform.rotation = Quaternion.Slerp(startRotation, endRotation, fractionOfJourney);
+            yield return null;
         }
         if ((transform.position - endPosition).magnitude <= 0.1f && Input.GetKeyDown(KeyCode.E))
         {
@@ -148,9 +122,32 @@ public class InterAction_Ctrl : MonoBehaviour
             GetComponentInChildren<CameraSettings>().enabled = true;
             transform.position = startPosition;
             transform.rotation = startRotation;
-            UnlockCursor();
             lerpPOV = false;
+            yield return null;
         }
     }
+    IEnumerator ToggleText()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (currentIndex < Obj_Cube.Length)
+            {
+                Time.timeScale = 0f; // 게임 일시 정지
+                pauseText.text = Obj_Cube[currentIndex]; // 현재 인덱스의 메시지 표시
+                pauseText.gameObject.SetActive(true); // 텍스트 활성화
+                currentIndex++; // 다음 메시지로 인덱스 증가
+                Debug.Log("E키 입력 대기");
 
+                //yield return new WaitUntil(() => Input.GetKeyDown(KeyCode.E)); // 사용자가 다시 E를 누를 때까지 대기
+                yield return null;
+            }
+            else
+            {
+                Time.timeScale = 1.0f; // 게임 재개
+                pauseText.gameObject.SetActive(false); // 텍스트 숨기기
+                toggleText = false;
+                yield return null;
+            }
+        }
+    }
 }
